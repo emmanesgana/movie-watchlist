aca817efe6151dcf75aba3969b88c1982cbb1be6
emmanesgana <emman.esgana@gmail.com>
2024-06-12 00:58:01 +0800

    update

--- index.js
+++ index.js
@@ -27,7 +27,6 @@ async function getMovies(value) {
         const resValue = await fetch(`https://www.omdbapi.com/?s=${value}&type=movie&apikey=c83db659`)
         const preData = await resValue.json()
         for (let movies of preData.Search) {
-            // console.log('movie', movies)
             const resImdbId = await fetch(`https://www.omdbapi.com/?i=${movies.imdbID}&type=movie&apikey=c83db659`)
             const postData = await resImdbId.json()
             moviesArr.push(postData)
@@ -40,26 +39,9 @@ async function getMovies(value) {
     }
 }
 
-function addToWatchlist(data) {
-    // add to watchlist
-    // console.log(data)
-    const watchListBtn = document.querySelectorAll('.watchlist-btn')
-    for (let i = 0; i < watchListBtn.length; i++) {
-        watchListBtn[i].addEventListener('click', (e) => {
-            e.preventDefault()
-            watchListBtn[i].innerHTML = `
-                <i class="fa-solid fa-circle-check" style="color: #26a269;"></i>
-                Saved
-            `
-            watchlistArr.push(data[i])
-            // console.log(watchlistArr)
-            localStorage.setItem('watchlistLocalStorage', JSON.stringify(watchlistArr))
-        })
-    }
-}
-
 function renderSearchResults(data) {
     for (let movie of data) {
+
         const {
             Poster,
             Title,
@@ -71,6 +53,89 @@ function renderSearchResults(data) {
         } = movie
 
         searchResults.innerHTML += `
+            <div class="container-movie">
+                <div class="poster">
+                    <img src=${Poster}  alt="movie-poster" class="poster"> 
+                </div>
+                <div class="movie-info">
+                    <div class="movie-data">
+                        <h3 class="movie-title">${Title}</h2>
+                        <p class="rating"><i class="fa-solid fa-star" style="color: #FFD43B;"></i> ${imdbRating}</p>
+                    </div>
+                    <div class="movie-details">
+                        <p class="runtime">${Runtime}</p>
+                        <p class="genre">${Genre}</p>
+                        <div id="btn">
+                        <button class="watchlist-btn">
+                            <i class="fa-solid fa-circle-plus" style="color: #000000;"></i>
+                            Watchlist
+                        </button>
+                        </div>
+                        
+                    </div>
+                    <p class="plot">${Plot}</p>
+                </div>
+            </div>
+            `
+    }
+}
+
+function renderFailedSearch() {
+    searchResults.innerHTML = `
+        <div class="container-search-failed">
+            <h3>Unable to find what you&#39;re looking for. Please try another search.</h1>
+        </div >
+        `
+}
+
+function addToWatchlist(data) {
+    const watchListBtn = document.querySelectorAll('.watchlist-btn')
+    for (let i = 0; i < watchListBtn.length; i++) {
+        watchListBtn[i].addEventListener('click', (e) => {
+            e.preventDefault()
+            // if()
+            watchListBtn[i].innerHTML = `
+                <i class="fa-solid fa-circle-check" style="color: #26a269;"></i>
+                Saved
+            `
+            watchListBtn[i].disabled = true
+            watchlistArr.push(data[i])
+            localStorage.setItem('watchlistArr', JSON.stringify(watchlistArr))
+        })
+    }
+}
+
+// watchlist 
+if (watchlistLocalStorage) {
+    watchlistArr = watchlistLocalStorage
+}
+
+if (watchList) {
+    if (watchlistArr.length > 0) {
+        renderWatchlist(watchlistArr)
+    } else {
+        watchList.innerHTML = `
+            <div class="container-empty" id="empty">
+                <p class="watchlist-empty">Your watchlist is a little empty</p>
+                <a href="./index.html" class="watchlist-return"><i class="fa-solid fa-circle-plus" style="color: #000000;"></i> Let's add some movies!</a>
+            </div>
+        `
+    }
+}
+
+function renderWatchlist(data) {
+    for (let movie of data) {
+        const {
+            Poster,
+            Title,
+            imdbRating,
+            Runtime,
+            Genre,
+            imdbID,
+            Plot
+        } = movie
+
+        watchList.innerHTML += `
         <div class="container-movie">
             <div class="poster">
                 <img src=${Poster}  alt="movie-poster" class="poster"> 
@@ -84,8 +149,8 @@ function renderSearchResults(data) {
                     <p class="runtime">${Runtime}</p>
                     <p class="genre">${Genre}</p>
                     <button class="watchlist-btn">
-                        <i class="fa-solid fa-circle-plus" style="color: #000000;"></i>
-                        Watchlist
+                        <i class="fa-solid fa-circle-minus" style="color: #000000;"></i>
+                        Remove
                     </button>
                 </div>
                 <p class="plot">${Plot}</p>
@@ -93,21 +158,4 @@ function renderSearchResults(data) {
         </div>
         `
     }
-}
-
-function renderFailedSearch() {
-    searchResults.innerHTML = `
-        <div class="container-search-failed">
-            <h3>Unable to find what you&#39;re looking for. Please try another search.</h1>
-        </div >
-        `
-}
-
-// watchlist
-if (watchlistLocalStorage) {
-    watchlistArr = watchlistLocalStorage
-}
-
-if (watchList) {
-
 }
